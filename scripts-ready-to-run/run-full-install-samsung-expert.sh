1f7dd5da189e5ea6f04bedd29d6fc6dcd3677a4e
André Gonzalez <lopescg@gmail.com>
2022-05-07 23:49:49 -0300

    separated nvidia from the full-install and removed skip tags

--- scripts-ready-to-run/run-full-install-samsung-expert.sh
+++
@@ -1,1 +0,0 @@
-ansible-pull --vault-password-file ~/.vault_key --ask-become -t "full-install,samsung-expert" -U https://github.com/andre-gonzalez/ansible.git
--- scripts-ready-to-run/samusung-expert.sh
+++ scripts-ready-to-run/samusung-expert.sh
@@ -1,1 +1,1 @@
-ansible-pull --vault-password-file ~/.vault_key --ask-become -t "full-install,samsung-expert" -U https://github.com/andre-gonzalez/ansible.git
+ansible-pull --vault-password-file ~/.vault_key --ask-become -t "full-install,samsung-expert,nvidia" -U https://github.com/andre-gonzalez/ansible.git
