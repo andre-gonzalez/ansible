834dcb07a9d4d52a54e829f3abf664490e69ba4c
André Gonzalez <lopescg@gmail.com>
2022-05-09 22:42:44 -0300

    transform processor from variable to tags with the tag never together

--- scripts-ready-to-run/samusung-expert.sh
+++ scripts-ready-to-run/samusung-expert.sh
@@ -1,1 +1,1 @@
-ansible-pull --vault-password-file ~/.vault_key --ask-become -t "full-install,samsung-expert,nvidia" -U https://github.com/andre-gonzalez/ansible.git
+ansible-pull --vault-password-file ~/.vault_key --ask-become -t "full-install,samsung-expert,nvidia,processor-intel" -U https://github.com/andre-gonzalez/ansible.git
