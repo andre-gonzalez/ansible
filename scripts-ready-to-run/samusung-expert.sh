dbbfcf6235f973e901b645b863470e8aa8937c5b
André Gonzalez <lopescg@gmail.com>
2022-05-31 22:12:04 -0300

    added the tag never to the collections-install

--- scripts-ready-to-run/samusung-expert.sh
+++ scripts-ready-to-run/samusung-expert.sh
@@ -1,1 +1,1 @@
-ansible-pull --vault-password-file ~/.vault_key --ask-become -t "full-install,samsung-expert,nvidia,processor-intel" -U https://github.com/andre-gonzalez/ansible.git
+ansible-pull --vault-password-file ~/.vault_key --ask-become -t "full-install,samsung-expert,nvidia,processor-intel,aur" -U https://github.com/andre-gonzalez/ansible.git
