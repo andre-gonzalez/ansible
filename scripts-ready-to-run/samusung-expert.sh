41851ed64782c08ec446e00b17ec5b28e5f07f6c
André Gonzalez <lopescg@gmail.com>
2022-06-29 08:41:32 -0300

    removed processor tags from ready scripts

--- scripts-ready-to-run/samusung-expert.sh
+++ scripts-ready-to-run/samusung-expert.sh
@@ -1,1 +1,1 @@
-ansible-pull --vault-password-file ~/.vault_key --ask-become -t "full-install,samsung-expert,nvidia,processor-intel,aur" -U https://github.com/andre-gonzalez/ansible.git
+ansible-pull --vault-password-file ~/.vault_key --ask-become -t "full-install,samsung-expert,nvidia,aur" -U https://github.com/andre-gonzalez/ansible.git
