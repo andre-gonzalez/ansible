dec8cb62fc03dc06b724df5dcc656c896ebfbb81
André Gonzalez <lopescg@gmail.com>
2022-08-09 13:03:11 -0500

    converted from txt to bat script

---
+++ windows-winget-apps.bat
@@ -0,0 +1,8 @@
+winget install --source winget --id Microsoft.PowerBI -e &&
+winget install --source winget --id DaxStudio.DaxStudio -e &&
+winget install --source winget --id BraveSoftware.BraveBrowser -e &&
+winget install --source winget --id Google.Drive -e &&
+winget install --source winget --id Spice.VDAgent -e &&
+winget install --source winget --id Microsoft.WindowsTerminal -e &&
+winget install --source winget --id Google.CloudSDK -e &&
+winget install --source winget --id DaxStudio.DaxStudio -e
