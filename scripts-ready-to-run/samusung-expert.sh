fe5cbf920f0889dcf84450e1b1ac7cd887002a8b
André Gonzalez <lopescg@gmail.com>
2024-11-24 18:01:22 -0600

    Add task to set up autologin in systemd and add autologin to samsung-expert script

--- scripts-ready-to-run/samusung-expert.sh
+++ scripts-ready-to-run/samusung-expert.sh
@@ -1,1 +1,1 @@
-ansible-pull --vault-password-file ~/.vault_key --ask-become -t "samsung_expert" -U https://github.com/andre-gonzalez/ansible.git
+ansible-pull --vault-password-file ~/.vault_key --ask-become -t "samsung_expert,autologin" -U https://github.com/andre-gonzalez/ansible.git
