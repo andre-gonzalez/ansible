9f6314dd5dc29d89dd29f7fa9ea19c0a92b4cf1b
André Gonzalez <lopescg@gmail.com>
2025-08-30 19:20:55 -0300

    add collection install before running ansible pull in the script ready to run

--- scripts-ready-to-run/samusung-expert.sh
+++ scripts-ready-to-run/samusung-expert.sh
@@ -1,1 +1,1 @@
-ansible-pull --vault-password-file ~/.vault_key --ask-become -t "samsung_expert,autologin,nvidia" --ask-become -U https://github.com/andre-gonzalez/ansible.git
+ansible-galaxy collection install kewlfft.aur && ansible-pull --vault-password-file ~/.vault_key --ask-become -t "samsung_expert,autologin,nvidia" --ask-become -U https://github.com/andre-gonzalez/ansible.git
